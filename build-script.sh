83858309c0d503231ed64b9933b6f4b176feb1a7
setavenger <setor.blagogee@gmx.de>
2023-02-26 12:23:00 +0100

    changed build script

--- build-script.sh
+++ build-script.sh
@@ -1,10 +1,11 @@
 # build react frontend
+npm install
 npm run build
 
 # build apps
 npx electron-packager --icon public/nostr-electron-icon.icns --ignore=build . --out=build
-npx electron-packager --icon public/nostr-electron-icon.icns --ignore=build --arch=arm64 . --out=build
+#npx electron-packager --icon public/nostr-electron-icon.icns --ignore=build --arch=arm64 . --out=build
 
 # make dmg files
-electron-installer-dmg --out=build/dmgs "build/Nostr Desktop-darwin-x64/Nostr Desktop.app" "Nostr Desktop Intel"
-electron-installer-dmg --out=build/dmgs "build/Nostr Desktop-darwin-arm64/Nostr Desktop.app" "Nostr Desktop Silicon"
+#electron-installer-dmg --out=build/dmgs "build/Nostr Desktop-darwin-x64/Nostr Desktop.app" "Nostr Desktop Intel"
+#electron-installer-dmg --out=build/dmgs "build/Nostr Desktop-darwin-arm64/Nostr Desktop.app" "Nostr Desktop Silicon"
